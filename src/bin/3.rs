bd2de236b6292781e310da670d869fb7c91a2379
Guillaume Pinot <guillaume.pinot@kardinal.ai>
2022-12-05 10:51:07 +0100

    day 3

---
+++ src/bin/3.rs
@@ -0,0 +1,61 @@
+use std::collections::HashSet;
+use std::io::BufRead;
+
+fn priority(&c: &u8) -> u32 {
+    (if c < b'a' {
+        c - b'A' + 27
+    } else {
+        c - b'a' + 1
+    }) as u32
+}
+
+fn part1() -> anyhow::Result<()> {
+    let mut score = 0;
+    for l in read()? {
+        let l = l?;
+        let (l, r) = l.split_at(l.len() / 2);
+        let l: HashSet<_> = l.iter().copied().collect();
+        let r = r.iter().copied().collect();
+        score += l.intersection(&r).map(priority).sum::<u32>();
+    }
+    println!("part1: {}", score);
+    Ok(())
+}
+
+fn get_three(
+    mut iter: impl Iterator<Item = anyhow::Result<Vec<u8>>>,
+) -> anyhow::Result<Option<[HashSet<u8>; 3]>> {
+    let Some(one) = iter.next().transpose()? else { return Ok(None) };
+    let Some(two) = iter.next().transpose()? else { return Ok(None) };
+    let Some(three) = iter.next().transpose()? else { return Ok(None) };
+    Ok(Some([
+        one.into_iter().collect(),
+        two.into_iter().collect(),
+        three.into_iter().collect(),
+    ]))
+}
+
+fn part2() -> anyhow::Result<()> {
+    let mut iter = read()?;
+    let mut score = 0;
+    loop {
+        let Some([one, two, three]) = get_three(&mut iter)? else { break };
+        score += one
+            .intersection(&two)
+            .filter(|c| three.contains(c))
+            .map(priority)
+            .sum::<u32>();
+    }
+    println!("part2: {}", score);
+    Ok(())
+}
+
+fn read() -> anyhow::Result<impl Iterator<Item = anyhow::Result<Vec<u8>>>> {
+    let f = std::io::BufReader::new(std::fs::File::open("data/input3.txt")?);
+    Ok(f.lines().map(|l| Ok(l.map(String::into_bytes)?)))
+}
+
+fn main() -> anyhow::Result<()> {
+    part1()?;
+    part2()
+}
