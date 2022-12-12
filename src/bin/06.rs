413c882e8c3a81253ceab96e14c49ce8cc5fa1e0
Guillaume Pinot <guillaume.pinot@kardinal.ai>
2022-12-12 17:38:18 +0100

    remove unit test in day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -16,11 +16,6 @@ fn find_first(buf: &[u8], size: usize) -> anyhow::Result<usize> {
 }
 
 fn main() -> anyhow::Result<()> {
-    assert_eq!(5, find_first(b"bvwbjplbgvbhsrlpgdmjqwftvncz", 4)?);
-    assert_eq!(6, find_first(b"nppdvjthqldpwncqszvftbrmjlhg", 4)?);
-    assert_eq!(10, find_first(b"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4)?);
-    assert_eq!(11, find_first(b"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4)?);
-
     let mut buf = vec![];
     std::fs::File::open("data/input6.txt")?.read_to_end(&mut buf)?;
     println!("part1: {}", find_first(&buf, 4)?);
