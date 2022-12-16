725e437ca7050a81053f35fa95caec4e8e646b6e
Guillaume Pinot <guillaume.pinot@kardinal.ai>
2022-12-16 17:35:24 +0100

    A rewrite using DP, still need 1 min in release to solve the 2 parts

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -1,114 +1,162 @@
 use regex::Regex;
-use std::collections::HashMap;
+use smol_str::SmolStr as String;
+use std::cmp::Ordering;
+use std::collections::{HashMap, HashSet};
 use std::io::{self, BufRead};
+use std::sync::Arc;
 
 lazy_static::lazy_static! {
     static ref RE: Regex =
         Regex::new(r"^Valve (..) has flow rate=(\d+); tunnels? leads? to valves? (.*)$").unwrap();
 }
 
+#[derive(Clone)]
 struct Room {
     rate: i32,
-    is_open: bool,
-    last_visit_releasing: i32,
     tunnels: HashMap<String, u32>,
 }
 
 #[derive(Default)]
 struct Search {
-    volcano: HashMap<String, Room>,
-    best: i32,
+    non_dominated: Vec<Arc<State>>,
+    q: std::collections::BinaryHeap<Arc<State>>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct State {
+    remaining: u32,
+    releasing: i32,
+    openned: HashSet<String>,
+    position: String,
+}
+impl State {
+    fn is_dominated_by(&self, other: &Self) -> bool {
+        if self.position != other.position {
+            return false;
+        }
+        self.remaining <= other.remaining
+            && self.releasing <= other.releasing
+            && self.openned.is_superset(&other.openned)
+    }
+}
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.remaining
+            .cmp(&other.remaining)
+            .then_with(|| self.releasing.cmp(&other.releasing))
+    }
 }
 
 impl Search {
-    fn simplify(&mut self) {
-        let zero_rate = self
-            .volcano
-            .iter()
-            .filter_map(|(t, r)| (t != "AA" && r.rate == 0).then_some(t.clone()))
-            .collect::<Vec<_>>();
-        for zero_room_name in zero_rate {
-            let zero_room = self.volcano.remove(&zero_room_name).unwrap();
-            for (room_name, room) in &mut self.volcano {
-                let Some(len) = room.tunnels.remove(&zero_room_name) else { continue };
-                for (tunnel, additional_len) in &zero_room.tunnels {
-                    if tunnel == room_name {
-                        continue;
-                    }
-                    let cur_len = room
-                        .tunnels
-                        .entry(tunnel.clone())
-                        .or_insert_with(|| u32::MAX);
-                    *cur_len = (*cur_len).min(len + additional_len);
-                }
-            }
+    fn push(&mut self, state: State) {
+        if self.non_dominated.iter().any(|s| state.is_dominated_by(s)) {
+            return;
         }
+        let state = Arc::new(state);
+        if state.remaining != 0 {
+            self.q.push(state.clone());
+        }
+        self.non_dominated.push(state);
     }
-    fn search(&mut self, room: &str, mut remaining: u32, mut releasing: i32) {
-        if remaining == 0 {
-            return;
-        }
-
-        let Some(cur) = self.volcano.get_mut(room) else { return };
-        if cur.last_visit_releasing >= releasing {
-            return;
-        }
-        let old_releasing = cur.last_visit_releasing;
-        cur.last_visit_releasing = releasing;
-
-        for (tunnel, &len) in cur.tunnels.clone().iter() {
-            if len > remaining {
-                continue;
+    fn search(volcano: &HashMap<String, Room>, remaining: u32) -> Self {
+        let mut s = Self::default();
+        s.push(State {
+            remaining,
+            releasing: 0,
+            openned: Default::default(),
+            position: "AA".into(),
+        });
+        while let Some(cur_state) = s.q.pop() {
+            let room = volcano.get(&cur_state.position).unwrap();
+            if room.rate > 0 && !cur_state.openned.contains(&cur_state.position) {
+                let mut openned = cur_state.openned.clone();
+                openned.insert(cur_state.position.clone());
+                s.push(State {
+                    remaining: cur_state.remaining - 1,
+                    releasing: cur_state.releasing + room.rate * (cur_state.remaining as i32 - 1),
+                    openned,
+                    position: cur_state.position.clone(),
+                });
             }
-            self.search(tunnel, remaining - len, releasing);
-        }
-
-        let cur = self.volcano.get_mut(room).unwrap();
-        let mut openned_here = false;
-        if !cur.is_open && cur.rate > 0 {
-            openned_here = true;
-            cur.is_open = true;
-            remaining -= 1;
-            releasing += remaining as i32 * cur.rate;
-            if self.best < releasing {
-                self.best = releasing;
-            }
-
-            for (tunnel, &len) in cur.tunnels.clone().iter() {
-                if len > remaining {
+            for (room, &dist) in &room.tunnels {
+                if dist > cur_state.remaining {
                     continue;
                 }
-                self.search(tunnel, remaining - len, releasing);
+                s.push(State {
+                    remaining: cur_state.remaining - dist,
+                    releasing: cur_state.releasing,
+                    openned: cur_state.openned.clone(),
+                    position: room.clone(),
+                });
             }
         }
+        s
+    }
+    fn best(&self) -> i32 {
+        self.non_dominated
+            .iter()
+            .map(|s| s.releasing)
+            .max()
+            .unwrap_or(0)
+    }
+}
 
-        let cur = self.volcano.get_mut(room).unwrap();
-        cur.last_visit_releasing = old_releasing;
-        if openned_here {
-            cur.is_open = false;
+fn simplify(volcano: &mut HashMap<String, Room>) {
+    let zero_rate = volcano
+        .iter()
+        .filter_map(|(t, r)| (t != "AA" && r.rate == 0).then_some(t.clone()))
+        .collect::<Vec<_>>();
+    for zero_room_name in zero_rate {
+        let zero_room = volcano.remove(&zero_room_name).unwrap();
+        for (room_name, room) in volcano.iter_mut() {
+            let Some(len) = room.tunnels.remove(&zero_room_name) else { continue };
+            for (tunnel, additional_len) in &zero_room.tunnels {
+                if tunnel == room_name {
+                    continue;
+                }
+                let cur_len = room
+                    .tunnels
+                    .entry(tunnel.clone())
+                    .or_insert_with(|| u32::MAX);
+                *cur_len = (*cur_len).min(len + additional_len);
+            }
         }
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut s = Search::default();
+    let mut volcano = HashMap::new();
     for l in io::BufReader::new(std::fs::File::open("data/input16.txt")?).lines() {
         let l = l?;
         let Some(c) = RE.captures(&l) else { anyhow::bail!("bad line {:?}", l) };
-        s.volcano.insert(
-            String::from(&c[1]),
+        volcano.insert(
+            c[1].into(),
             Room {
                 rate: c[2].parse()?,
-                is_open: false,
-                last_visit_releasing: -1,
                 tunnels: c[3].split(", ").map(|t| (t.into(), 1)).collect(),
             },
         );
     }
 
-    s.simplify();
-    s.search("AA", 30, 0);
-    println!("Part1: {}", s.best);
+    simplify(&mut volcano);
+    let s = Search::search(&volcano, 30);
+    println!("Part1: {}", s.best());
+
+    let s = Search::search(&volcano, 26);
+    let mut releasing = 0;
+    for (i, s1) in s.non_dominated.iter().enumerate() {
+        for s2 in &s.non_dominated[0..i] {
+            if s1.openned.is_disjoint(&s2.openned) {
+                releasing = releasing.max(s1.releasing + s2.releasing);
+            }
+        }
+    }
+    println!("Part1: {}", releasing);
 
     Ok(())
 }
